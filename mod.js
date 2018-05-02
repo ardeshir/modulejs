ea4c82c608354d8378c5ff313867c4d6e2fb3730
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-05-02 14:37:34 +0000

    first ...

---
+++ mod.js
@@ -0,0 +1,61 @@
+
+    function Person(firstName, lastName, age) {
+          this.firstName = firstName;
+          this.lastName = lastName;
+          this.age      = age;
+          console.log(this)
+    }
+     
+     let p = new Person("John", "Smith", 43);
+     console.log(p);
+     
+     function Pet(name) {
+         this.name = name;
+     }
+     
+     Pet.prototype.eat = function()  {
+         console.log(`${this.name} is eating...`);
+     }
+     
+     function Dog(name, breed) {
+         Pet.call(this, name)
+         this.breed = breed ;
+     }
+     
+     Dog.prototype = Object.create(Pet.prototype);
+     
+     Dog.prototype.play = function() {
+         console.log(`${this.name} is playing...`);
+     }
+     
+     var max = new Dog("Max", "Golden Retriever");
+     max.play();
+     max.eat();
+    
+console.log(max)
+
+class Pet1 {
+      constructor(name) {
+          this.name = name;
+      }
+      
+      eat() {
+           console.log(`${this.name} is eating...`);
+      }
+}
+
+class Dog1 extends Pet1 {
+          constructor(name, breed) {
+          super(name);
+          this.breed = breed;
+      }
+      
+      play() {
+          console.log(`${this.name} is playing ...`)
+      }
+}
+
+let max2 = new Dog1("max2", "Another type");
+max2.play();
+max2.eat();
+console.log(max2)
