f778a8c84f427d73df618fba1f1b209ef01dd2bf
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-05-02 17:42:32 +0000

    adding module pattern (old school)

--- module-pattern.js
+++ module-pattern.js
@@ -2,9 +2,12 @@
 
 function ModuleLoader() {
     
+    //  store all the modules of the application
     var modules = {};
     
-    function getInstance(name) {
+    //  get all the modules store an insance of the module
+    
+    function getModule(name) {
         if (modules[name]) {
             return modules[name];
         } else {
@@ -12,53 +15,71 @@ function ModuleLoader() {
         }
     }
         
-    function define(name, dependencies, instance) {
+    //  once you find your modules, register the instance of the module
+    //  and add it to the stored list of modules
+    
+    function register(name, dependencies, module) {
         
         if (modules[name]) {
             throw new Error(`Module ${name} already defined`);
         } else {
             
-            var depsInstances = dependencies.map((dep) => {
-                 return getInstance(dep);
-            });
+            //  grab the instances of the modules and add them to the module list.
+            //  apply adds the concrete modules using the apply to get "this"
+            //  arry of implomentations of modules
             
-            modules[name] = instance.apply(instance, depsInstances);
+            var depsModules = dependencies.map((dep) => {
+                 return getModule(dep);
+            });
+            // pass the moduel and the implomentations 
+            modules[name] = module.apply(module, depsModules);
         }
     }
-    
+     //  expose the two functions needed to use the Module Loader
     return {
-        getInstance: getInstance,
-        define: define
+        getModule: getModule,
+        register: register
     };
 }
 
+// Example of using two modules
+// Create an instance of ModuleLoader
+
 var loader = ModuleLoader();
 
-loader.define("HttpClient", [], function HttpClient() {
+loader.register("HttpClient", [], function HttpClient() {
+    
+    // Using fetch API as an example
     
     function get(url) {
-        
         return fetch(url).then((response) => response.json());
     }
+    
+    // Must return public API from all modules registers
     
     return {
         get: get     
     };
+    
 });
 
-loader.define("PostManager", ["HttpClient"], function PostManager(httpClient) {
-    
+// Second example PostManager depends on the HttpClient defined above, and it is added othe [ ] array
+
+loader.register("PostManager", ["HttpClient"], function PostManager(httpClient) {
+
+      // GetPosts used the httpClient's get function to pull the posts
       function getPosts() {
           httpClient.get("https://jsonplaceholder.typicode.com/posts").then((posts) => {
               console.log("Posts ", posts);
           });
       }
     
+    // Expose API of the most manager getPosts 
     return {
         getPosts: getPosts
     };
     
 });
 
-let postManager = loader.getInstance("PostManager");
+let postManager = loader.getModule("PostManager");
 postManager.getPosts();
